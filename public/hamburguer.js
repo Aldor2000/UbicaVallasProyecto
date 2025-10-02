c381a645a90c893dff95679a304b16cd5f837450
Aldor2000 <alejandro.torrico1601@gmail.com>
2025-10-02 14:13:54 -0400

    revise los estilos en un solo archivo

---
+++ public/hamburguer.js
@@ -0,0 +1,7 @@
+// hamburger.js
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".navbar-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
